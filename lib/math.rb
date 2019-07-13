c197f7da34da4f43bd14f34a16e2e19eb89c8f1a
Randall Taylor <randyisbusy@gmail.com>
2019-07-13 22:23:40 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -11,7 +11,7 @@ def division(num1, num2)
 end
 
 def multiplication(num1, num2)
-  num1 ** num2
+  num1 * num2
 end
 
 def modulo(num1, num2)
@@ -24,4 +24,5 @@ end
 
 def order_of_operation(num1, num2, num3, num4)
   #Hint:  __ + (( __ * __ ) / __ )
+  num1 + ((num2 * num3) / num4)
 end
